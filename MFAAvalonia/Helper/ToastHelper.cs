9e53c38ca0375e09e151e88942f152e71145d913
SweetSmellFox <1254371679@qq.com>
2025-03-16 11:16:18 +0800

    feat: 弹窗辅助器

---
+++ MFAAvalonia/Helper/ToastHelper.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Controls.Notifications;
+using SukiUI.Toasts;
+using System;
+
+namespace MFAAvalonia.Helper;
+
+public static class ToastHelper
+{
+    public static SukiToastBuilder CreateToastByType(NotificationType toastType, string title = "", string content = "", int duration = 3)
+    {
+        return Instances.ToastManager.CreateToast()
+            .WithTitle(title)
+            .WithContent(
+                content)
+            .OfType(toastType).Dismiss().After(TimeSpan.FromSeconds(duration))
+            .Dismiss().ByClicking();
+    }
+
+    public static void Success(string title = "", string content = "", int duration = 3)
+    {
+        DispatcherHelper.RunOnMainThread(() => CreateToastByType(NotificationType.Success, title, content, duration).Queue());
+    }
+
+    public static void Info(string title = "", string content = "", int duration = 3)
+    {
+        DispatcherHelper.RunOnMainThread(() => CreateToastByType(NotificationType.Information, title, content, duration).Queue());
+    }
+
+    public static void Warn(string title = "", string content = "", int duration = 3)
+    {
+        DispatcherHelper.RunOnMainThread(() => CreateToastByType(NotificationType.Warning, title, content, duration).Queue());
+    }
+
+    public static void Error(string title = "", string content = "", int duration = 3)
+    {
+        DispatcherHelper.RunOnMainThread(() => CreateToastByType(NotificationType.Error, title, content, duration).Queue());
+    }
+}

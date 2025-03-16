b9e8dc6d5089407b67b3c0485c65fb946c3ec727
SweetSmellFox <1254371679@qq.com>
2025-03-16 11:14:05 +0800

    feat: 新增adb编辑弹窗

---
+++ MFAAvalonia/ViewModels/UsersControls/AdbEditorDialogViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MaaFramework.Binding;
+using MFAAvalonia.Extensions;
+using MFAAvalonia.Helper;
+using MFAAvalonia.Views.UserControls;
+using SukiUI.Dialogs;
+using System.Threading.Tasks;
+
+namespace MFAAvalonia.ViewModels.UsersControls;
+
+public partial class AdbEditorDialogViewModel : ObservableObject
+{
+    [ObservableProperty] private string _adbName = "Emulator".ToLocalization();
+    [ObservableProperty] private string _adbPath = string.Empty;
+    [ObservableProperty] private string _adbSerial = string.Empty;
+    [ObservableProperty] private string _adbConfig = "{}";
+    public ISukiDialog Dialog { get; set; }
+    public AdbEditorDialogViewModel(AdbDeviceInfo? info, ISukiDialog dialog)
+    {
+        AdbName = info?.Name ?? AdbName;
+        AdbPath = info?.AdbPath ?? AdbPath;
+        AdbSerial = info?.AdbSerial ?? AdbSerial;
+        AdbConfig = info?.Config ?? AdbConfig;
+        Dialog = dialog;
+    }
+
+    [RelayCommand]
+    async private Task Load()
+    {
+        var storageProvider = Instances.RootView.StorageProvider;
+
+        // 配置文件选择器选项
+        var options = new FilePickerOpenOptions
+        {
+            Title = "LoadFileTitle".ToLocalization(),
+            FileTypeFilter =
+            [
+                new FilePickerFileType("AllFilter".ToLocalization())
+                {
+                    Patterns = ["*"] // 支持所有文件类型
+                }
+            ]
+        };
+        
+        var result = await storageProvider.OpenFilePickerAsync(options);
+
+        // 处理选择结果
+        if (result is { Count: > 0 } && result[0].TryGetLocalPath() is { } path)
+        {
+            AdbPath = path;
+        }
+    }
+
+    [RelayCommand]
+    public void Save()
+    {
+        Instances.TaskQueueViewModel.Devices = [Output];
+        Instances.TaskQueueViewModel.CurrentDevice = Output;
+        Dialog.Dismiss();
+    }
+
+    public AdbDeviceInfo Output => new(AdbName, AdbPath, AdbSerial, AdbScreencapMethods.Default,
+        AdbInputMethods.MinitouchAndAdbKey, AdbConfig);
+}
---
+++ MFAAvalonia/Views/UserControls/AdbEditorDialogView.axaml.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
+using MaaFramework.Binding;
+using MFAAvalonia.Extensions;
+using MFAAvalonia.Helper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFAAvalonia.Views.UserControls;
+
+public partial class AdbEditorDialogView : UserControl
+{
+    public AdbEditorDialogView()
+    {
+        InitializeComponent();
+    }
+    
+    private void File_DragOver(object sender, DragEventArgs e)
+    {
+        e.Handled = true;
+    }
+    
+    private void File_Drop(object sender, DragEventArgs e)
+    {
+        if (!e.Data.Contains(DataFormats.Files))
+        {
+            return;
+        }
+        
+        var storageItems = e.Data.GetFiles()?.ToList(); // 获取 IStorageItem 集合
+        if (storageItems?.Count > 0 && sender is TextBox textBox)
+        {
+            var firstFile = storageItems[0].TryGetLocalPath(); // 提取本地路径
+            textBox.Text = firstFile ?? string.Empty;
+        }
+    }
+}

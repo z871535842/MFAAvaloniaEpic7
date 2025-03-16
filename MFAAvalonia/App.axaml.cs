fb71dc995d3598b1187cd56b6093622f5ef48c7b
SweetSmellFox <1254371679@qq.com>
2025-03-16 11:14:35 +0800

    refactor: 重写依赖注入

--- MFAAvalonia/App.axaml.cs
+++ MFAAvalonia/App.axaml.cs
@@ -1,75 +1,145 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using MFAAvalonia.Extensions;
 using MFAAvalonia.Helper;
 using MFAAvalonia.ViewModels;
+using MFAAvalonia.ViewModels.Pages;
+using MFAAvalonia.ViewModels.UsersControls;
+using MFAAvalonia.ViewModels.UsersControls.Settings;
 using MFAAvalonia.ViewModels.Windows;
+using MFAAvalonia.Views.Pages;
+using MFAAvalonia.Views.UserControls;
+using MFAAvalonia.Views.UserControls.Settings;
 using MFAAvalonia.Views.Windows;
-using Microsoft.Extensions.Configuration;
+using MFAWPF.Helper;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using SukiUI.Dialogs;
+using SukiUI.Toasts;
 using System;
 using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MFAAvalonia;
 
 public partial class App : Application
 {
-    private static readonly IHost Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
-        .ConfigureAppConfiguration(c =>
-        {
-            var basePath =
-                Path.GetDirectoryName(AppContext.BaseDirectory)
-                ?? throw new DirectoryNotFoundException(
-                    "Unable to find the base directory of the application."
-                );
-            _ = c.SetBasePath(basePath);
-        })
-        .ConfigureServices(
-            (context, services) =>
-            {
-                // App Host
-
-
-                // Main window with navigation
-                _ = services.AddSingleton<RootView>();
-                _ = services.AddSingleton(Instances.RootViewModel);
-                // _ = services.AddSingleton<Views.Pages.DataPage>();
-                // _ = services.AddSingleton<ViewModels.DataPageViewModel>();
-                // _ = services.AddSingleton<Views.Pages.SettingsPage>();
-                // _ = services.AddSingleton<ViewModels.SettingsViewModel>();
-
-            }
-        )
-        .Build();
-
     /// <summary>
     /// Gets services.
     /// </summary>
-    public static IServiceProvider Services => Host.Services;
+    public static IServiceProvider Services { get; private set; }
 
     public override void Initialize()
     {
-        AvaloniaXamlLoader.Load(this);   
+        AvaloniaXamlLoader.Load(this);
         LanguageHelper.Initialize();
-    }
+        Configuration.ConfigurationManager.Initialize();
+        var cracker = new AvaloniaMemoryCracker();
+        cracker.Cracker();
 
+        TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException; //Task线程内未捕获异常处理事件
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException; //非UI线程内未捕获异常处理事件
+        Dispatcher.UIThread.UnhandledException += OnDispatcherUnhandledException; //UI线程内未捕获异常处理事件
+    }
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var serviceProvider = Services;
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = serviceProvider.GetRequiredService<RootView>();
-            desktop.MainWindow.DataContext = serviceProvider.GetRequiredService<RootViewModel>();
+            var services = new ServiceCollection();
+
+            services.AddSingleton(desktop);
+            
+            ConfigureServices(services);
+
+            var views = ConfigureViews(services);
+            
+            Services = services.BuildServiceProvider();
+
+            DataTemplates.Add(new ViewLocator(views));
+
+            desktop.MainWindow = views.CreateView<RootViewModel>(Services) as Window;
         }
-        Instances.RootViewModel.Greeting = "测试";
-        Console.WriteLine(Instances.RootViewModel.Greeting);
+
         base.OnFrameworkInitializationCompleted();
     }
-    
+
+    private static ViewsHelper ConfigureViews(ServiceCollection services)
+    {
+        return new ViewsHelper()
+
+            // Add main view
+            .AddView<RootView, RootViewModel>(services)
+
+            // Add pages
+            .AddView<TaskQueueView, TaskQueueViewModel>(services)
+            .AddView<SettingsView, SettingsViewModel>(services)
+
+            // Add additional views
+            .AddView<AdbEditorDialogView, AdbEditorDialogViewModel>(services)
+            .AddView<ConnectSettingsUserControl,ConnectSettingsUserControlModel>(services)
+            .AddView<GuiSettingsUserControl,GuiSettingsUserControlModel>(services);
+    }
+
+    private static void ConfigureServices(ServiceCollection services)
+    {
+        services.AddSingleton<ISukiToastManager, SukiToastManager>();
+        services.AddSingleton<ISukiDialogManager, SukiDialogManager>();
+    }
+
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        try
+        {
+            e.Handled = true;
+            LoggerHelper.Error(e.Exception);
+            ErrorView.ShowException(e.Exception);
+        }
+        catch (Exception ex)
+        {
+            //此时程序出现严重异常，将强制结束退出
+            LoggerHelper.Error(ex.ToString());
+            ErrorView.ShowException(ex, true);
+        }
+    }
+
+
+    void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        var sbEx = new StringBuilder();
+        if (e.IsTerminating)
+            sbEx.Append("非UI线程发生致命错误");
+        else
+            sbEx.Append("非UI线程异常：");
+        if (e.ExceptionObject is Exception ex)
+        {
+            ErrorView.ShowException(ex);
+            sbEx.Append(ex.Message);
+        }
+        else
+        {
+            sbEx.Append(e.ExceptionObject);
+        }
+        LoggerHelper.Error(sbEx);
+    }
+
+    void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+    {
+        //task线程内未处理捕获
+        LoggerHelper.Error(e.Exception);
+        ErrorView.ShowException(e.Exception);
+        foreach (var item in e.Exception.InnerExceptions)
+        {
+            LoggerHelper.Error(string.Format("异常类型：{0}{1}来自：{2}{3}异常内容：{4}",
+                item.GetType(), Environment.NewLine, item.Source,
+                Environment.NewLine, item.Message));
+        }
+
+        e.SetObserved(); //设置该异常已察觉（这样处理后就不会引起程序崩溃）
+    }
 }

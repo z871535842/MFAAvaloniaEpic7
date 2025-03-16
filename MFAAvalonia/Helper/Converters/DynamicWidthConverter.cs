a5ed93d21292818621cef5f80341f0293ab9dd85
SweetSmellFox <1254371679@qq.com>
2025-03-16 11:17:21 +0800

    feat: 辅助转换类

---
+++ MFAAvalonia/Helper/Converters/DynamicWidthConverter.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MFAAvalonia.Helper.Converters;
+
+public class DynamicWidthConverter : IMultiValueConverter
+{
+    public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var minWidth = 100d;
+        if (values.Count < 3 || !double.TryParse(values[0]?.ToString(), out double parentWidth) || !double.TryParse(values[1]?.ToString(), out double siblingWidth) || !double.TryParse(values[2]?.ToString(), out minWidth))
+            return minWidth;
+
+        var availableWidth = parentWidth - siblingWidth - 15;
+        if (availableWidth < minWidth)
+             minWidth = Math.Max(parentWidth - 15, minWidth);
+        return Math.Max(availableWidth, minWidth);
+    }
+}
